e9caf96f1c37041936f829e3758ff0aefff80704
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-18 16:15:33 -0300

    2

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,8 @@ class Bookmark < ApplicationRecord
   validates :comment, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
+  # validates_uniqueness_of :movie, scope: :list
+
+  validates :movie, uniqueness: { scope: :list }
+
 end
