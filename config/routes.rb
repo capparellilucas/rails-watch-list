351c8f6bc275d4328b85b8973f3a73988cc35185
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-19 13:55:41 -0300

    2

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  resources :lists, only: [:index, :show, :new, :create]
+  root to: "lists#index"
+  resources :lists, only: [:show, :new, :create]
 end
