c81be51ba55e4bac035bd4211e620af1d6a6f3fd
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-20 16:06:29 -0300

    2

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,18 +4,26 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  def show
-    @lists = List.find(params[:id])
-  end
-
   def new
     @lists = List.new()
   end
 
-  def create
-    @list = List.new(params[:list])
-    @list.save # Will raise ActiveModel::ForbiddenAttributesError
+  def show
+    @lists = List.find(params[:id])
   end
 
+
+  def create
+    @list = List.new(list_params)
+    @list.save # Will raise ActiveModel::ForbiddenAttributesError
+
+    redirect_to lists_path
+  end
+
+  private
+
+def list_params
+  params.require(:list).permit(:name)
+end
 
 end
